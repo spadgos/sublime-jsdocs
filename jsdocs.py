1a64a82ff07211b9cfae3a8d7142c4ae82ba8535
Nick Fisher <spadgos@gmail.com>
2012-08-08 21:42:50 +0200

    Fixes actionscript docblocks

--- jsdocs.py
+++ jsdocs.py
@@ -580,7 +580,7 @@ class JsdocsActionscript(JsdocsParser):
         res = re.search(
             #   fnName = function,  fnName : function
             '(?:(?P<name1>' + self.settings['varIdentifier'] + ')\s*[:=]\s*)?'
-            + 'function(?:\s+(?P<getset>[gs]et))'
+            + 'function(?:\s+(?P<getset>[gs]et))?'
             # function fnName
             + '(?:\s+(?P<name2>' + self.settings['fnIdentifier'] + '))?'
             # (arg1, arg2)
